2f26cc9a2a2b08f159b36e1585c5b9ac75399fe1
Kamber7 <jenya.yunashev41@gmail.com>
2026-05-01 15:07:17 +0500

    fix:yaml files

--- service/src/main/java/ru/rentplatform/gatewayservice/config/SecurityConfig.java
+++ service/src/main/java/ru/rentplatform/gatewayservice/config/SecurityConfig.java
@@ -44,6 +44,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/catalog/items/*/similar").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/catalog/items/*/deal-info").permitAll()
 
+                        .requestMatchers(HttpMethod.GET, "/api/reviews/users/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/reviews/items/*").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
